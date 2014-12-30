1f8bc47a8450af45c00d43e7ad0789515c529e56
acer <geeknik96@gmail.com>
2014-12-30 23:57:43 +0300

    test 2

--- main.c
+++ main.c
@@ -5,29 +5,27 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+
 int main(int argc, char **argv) //client
 {
     printf("Connection...");
 
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in address;
+    
     address.sin_addr.s_addr = htonl(0xC0A800F9);
     address.sin_family = AF_INET;
     address.sin_port = htons(31415);
 
     char buff[1024], q[] = "!exit";
 
-    //printf("%d - %d\n %d - %d\n %d - %d", address.sin_family, AF_INET, address.sin_port, htons(31415), address.sin_addr.s_addr ,htonl(0xC0A800F9));
-
-
     if (connect(client_socket, &address, sizeof address) < 0) {
         printf("Connection error!");
-     //   exit(-1);
+        exit(-1);
     }
 
     do {
-        fflush(stdout);
-        scanf("%60s", buff);
+        scanf("%1023s", buff);
         send(client_socket, buff, strlen(buff), 0);
     } while(strcmp(buff, q) != 0);
 
