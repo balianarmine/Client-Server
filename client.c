82db979aa3c14b4a98203d0792ae5b344e338cb8
acer <geeknik96@gmail.com>
2015-01-01 20:31:31 +0300

    add client.c

--- client.c
+++ client.c
@@ -7,26 +7,20 @@
 
 int main(int argc, char **argv) //client
 {
-  
-
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in address;
-    
-    address.sin_addr.s_addr = htonl(0xC0A800F9);
-    address.sin_family = AF_INET;
-    address.sin_port = htons(31415);
-
     char buff[1024], q[] = "!exit";
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in address = {AF_INET, htons(50003), inet_addr(argv[1])};
 
     if (connect(client_socket, &address, sizeof address) < 0) {
-        printf("Connection error!");
+        perror("Connection error");
         exit(-1);
     }
 
+    printf("Ready!\n");
     do {
-        scanf("%1023s", buff);
+        scanf("%s", buff);
         send(client_socket, buff, strlen(buff), 0);
-    } while(strcmp(buff, q) != 0);
+    } while(strcmp(buff, q));
 
     close(client_socket);
     return 0;
